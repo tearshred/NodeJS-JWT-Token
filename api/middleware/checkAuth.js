1e33f632c53d3caceb157b25b40483c613ef20c6
Centurion <centurion@Centurions-iMac.attlocal.net>
2019-08-22 16:30:02 -0700

    cleaned up code

--- api/middleware/checkAuth.js
+++ api/middleware/checkAuth.js
@@ -13,7 +13,4 @@ module.exports = (req, res, next) => {
             message: "Auth failed"
         });
     }
-    
-    
-    
 }; 
--- api/routes/autorizedUser.js
+++ api/routes/autorizedUser.js
@@ -55,10 +55,29 @@ mongoose.connect(dbUrl, {useNewUrlParser: true, useCreateIndex: true}, (err, db)
         db.close();
       });
   });
-
-  
   });
 });
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+      // Split at the space
+      const bearer = bearerHeader.split(' ');
+      // Get token from array
+      const bearerToken = bearer[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      next();
+    } else {
+      // Forbidden
+      res.sendStatus(403);
+    }
+  
+  }
 
 router.post('/signup', (req, res, next) => {
   User.find({ email: req.body.email })
